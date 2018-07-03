8802c791601c73a168d05e9365c381514fc30f6e
Samuel Rapana Betio <samuelbetio@users.noreply.github.com>
2018-07-03 14:58:38 +0800

    Delete video_avid.js

--- js/video_avid.js
+++
@@ -1,7 +0,0 @@
-$().ready(function() {
-    $('video.avid').each(function () {
-        if ($(this).data('url')) {
-            $(this).attr('controlsList','nodownload').html('<source src="http://cdn.ahacdu.com' + $(this).data('url') + '.mp4"><source src="http://cdn.ahacdu.com' + $(this).data('url') + '.webm">');
-        }
-    });
-});
